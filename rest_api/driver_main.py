5148e481cc005658771cf60d7461a7b2f9ed2a51
Chris Fernando <c.fernando@optusnet.com.au>
2021-07-08 12:53:43 +0000

    Working code for stocks.  About to break everything for sectors

--- rest_api/driver_main.py
+++ rest_api/driver_main.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-
+from datetime import date
 from fastapi import Depends, FastAPI, HTTPException, Query
 from fastapi.exception_handlers import (
     http_exception_handler,
@@ -49,79 +49,206 @@ async def validation_exception_handler(request, exc):
     return await request_validation_exception_handler(request, exc)
 
 
-# request routing
+# hardcoded routes
 @app.get("/")
 def main():
     return RedirectResponse(url="/docs/")
 
 
-# USE CASE 1
+@app.get("/quote", response_model=List[str])
+def main():
+    return ["stock", "sector"]
+
+
+# SECTOR QUOTE LITERAL RETURNS
+@app.get("/quote/stock", response_model=List[str])
+def main():
+    return ["intraday", "daily", "weekly"]
+
+
+@app.get("/quote/stock/intraday", response_model=str)
+def main():
+    return "Not implemented"
+
+
+@app.get("/quote/stock/daily", response_model=List[str])
+def main():
+    return "Not implemented"
+
+
+# STOCK QUOTE LITERAL RETURNS
+@app.get("/quote/sector", response_model=List[str])
+def main():
+    return ["intraday", "daily", "weekly"]
+
+
+@app.get("/quote/sector/intraday", response_model=str)
+def main():
+    return "Not implemented"
+
+
+@app.get("/quote/sector/daily", response_model=List[str])
+def main():
+    return "Not implemented"
+
+
+# STOCK USE CASE 1
 # list stocks in database
 # takes optional filter as a request query: 'stock_codes' which list of stock codes
-@app.get("/stocks/", response_model=List[entity_schemas.Stock])
-def show_records(filters: Optional[usecase_rules.stock_filter] = None, db: Session = Depends(get_db)):
+@app.get("/stock/", response_model=List[entity_schemas.Stock])
+def show_records(
+    filters: Optional[usecase_rules.stock_filter] = None, db: Session = Depends(get_db)
+):
     if filters == None:
         # catches no get query
-        records = db.query(adapter_models.Stock).all()
-        return records
+        return db.query(adapter_models.Stock).all()
     elif filters.stock_code == None:
-        raise HTTPException(status_code=400, detail="Bad request. Acceptable Query parameter is 'stock_code'")
+        raise HTTPException(
+            status_code=400,
+            detail="Bad request. Acceptable Query parameter is 'stock_code'",
+        )
     else:
-        records = db.query(adapter_models.Stock).filter(
-            adapter_models.Stock.stock_code.in_(filters.stock_code)
-        ).all()
-        #records_dict = [u.__dict__ for u in records]
-        return records
+        return (
+            db.query(adapter_models.Stock)
+            .filter(adapter_models.Stock.stock_code.in_(filters.stock_code))
+            .all()
+        )
 
 
-# USE CASE 2
+# STOCK USE CASE 2
 # gets a specific stock code
-# does not accept request queries 
-@app.get("/stocks/{search_stock}", response_model=List[entity_schemas.Stock])
+# does not accept request queries
+@app.get("/stock/{search_stock}", response_model=List[entity_schemas.Stock])
 def show_records(search_stock: str, db: Session = Depends(get_db)):
-    records = db.query(adapter_models.Stock).filter(
-        adapter_models.Stock.stock_code==search_stock
-    ).all()
-    #records_dict = [u.__dict__ for u in records]
-    return records
+    return (
+        db.query(adapter_models.Stock)
+        .filter(adapter_models.Stock.stock_code == search_stock)
+        .all()
+    )
 
 
-# USE CASE 3
+# STOCK USE CASE 3
 # gets a list of the weekly quotes for a specific stock code {search_stock}
-# needs to be modified to accept request query, which would be a list of dates
-@app.get("/stocks/{search_stock}/quotes/weekly", response_model=List[entity_schemas.Weekly_Stock_Quotes])
-def show_records(search_stock: str, filters: Optional[usecase_rules.date_filter] = None, db: Session = Depends(get_db)):
+@app.get(
+    "/stock/{search_stock}/quotes/weekly",
+    response_model=List[entity_schemas.Weekly_Stock_Quotes],
+)
+def show_records(
+    search_stock: str,
+    filters: Optional[usecase_rules.date_filter] = None,
+    db: Session = Depends(get_db),
+):
     if filters == None:
         # catches no get query
-        records = db.query(adapter_models.Weekly_Stock_Quotes).filter(
-            adapter_models.Weekly_Stock_Quotes.stock_code==search_stock
-        ).all()
-        #records_dict = [u.__dict__ for u in records]
-        return records
+        return (
+            db.query(adapter_models.Weekly_Stock_Quotes)
+            .filter(adapter_models.Weekly_Stock_Quotes.stock_code == search_stock)
+            .all()
+        )
     else:
         filters.processSearchDates()
         if filters.start == None or filters.end == None:
             # they sent something weird and the constructor wasn't able to translate it into something usable
-            raise HTTPException(status_code=400, detail="Bad request. Acceptable Query parameters are 'start_date' and/or 'end_date', or 'period'")
+            raise HTTPException(
+                status_code=400,
+                detail="Bad request. Acceptable Query parameters are 'start_date' and/or 'end_date', or 'period'",
+            )
         else:
             # they've set filters
-            records = db.query(adapter_models.Weekly_Stock_Quotes).filter(
-                adapter_models.Weekly_Stock_Quotes.stock_code==search_stock
-            ).filter(
-                adapter_models.Weekly_Stock_Quotes.quote_date >= filters.start,
-                adapter_models.Weekly_Stock_Quotes.quote_date < filters.end,
-            ).all()
+            return (
+                db.query(adapter_models.Weekly_Stock_Quotes)
+                .filter(adapter_models.Weekly_Stock_Quotes.stock_code == search_stock)
+                .filter(
+                    adapter_models.Weekly_Stock_Quotes.quote_date >= filters.start,
+                    adapter_models.Weekly_Stock_Quotes.quote_date < filters.end,
+                )
+                .all()
+            )
 
-            return records
 
-# USE CASE 4
+# STOCK USE CASE 4
 # gets a specific quote (specific stock, specific date)
 # does not accept request queries
-@app.get("/stocks/{search_stock}/quotes/weekly/{search_date}", response_model=List[entity_schemas.Weekly_Stock_Quotes])
+@app.get(
+    "/stock/{search_stock}/quotes/weekly/{search_date}",
+    response_model=List[entity_schemas.Weekly_Stock_Quotes],
+)
 def show_records(search_stock: str, search_date: str, db: Session = Depends(get_db)):
-    
-    records = db.query(adapter_models.Weekly_Stock_Quotes).\
-        filter(adapter_models.Weekly_Stock_Quotes.stock_code==search_stock). \
-        filter(adapter_models.Weekly_Stock_Quotes.quote_date==search_date)
-    records_dict = [u.__dict__ for u in records]
-    return records_dict
+
+    return (
+        db.query(adapter_models.Weekly_Stock_Quotes)
+        .filter(adapter_models.Weekly_Stock_Quotes.stock_code == search_stock)
+        .filter(adapter_models.Weekly_Stock_Quotes.quote_date == search_date)
+        .all()
+    )
+
+
+# STOCK USE CASE 5
+# gets all of the dates we have weekly quotes for
+# takes optional filter as a request query: 'stock_codes' which list of stock codes
+@app.get("/quote/stock/weekly", response_model=List)
+def show_records(
+    filters: Optional[usecase_rules.stock_filter] = None, db: Session = Depends(get_db)
+):
+    if filters == None:
+        # catches no get query
+        # can't just return .distinct.all() because that would be a dict with quote_date as key and the date as val
+        # whereas what we really want here is a flat list of dates
+        returnList = []
+        records = (
+            db.query(adapter_models.Weekly_Stock_Quotes.quote_date).distinct().all()
+        )
+        [returnList.append(u.quote_date) for u in records]
+        return returnList
+    elif filters.stock_code == None:
+        raise HTTPException(
+            status_code=400,
+            detail="Bad request. Acceptable Query parameter is 'stock_code'",
+        )
+    else:
+        returnList = []
+        records = (
+            db.query(adapter_models.Weekly_Stock_Quotes.quote_date)
+            .filter(
+                adapter_models.Weekly_Stock_Quotes.stock_code.in_(filters.stock_code)
+            )
+            .distinct()
+            .all()
+        )
+        [returnList.append(u.quote_date) for u in records]
+        return returnList
+
+
+# STOCK USE CASE 6
+# gets the quotes for all (or filtered list of) stock codes
+# takes optional filter as a request query: 'stock_codes' which list of stock codes
+# todo: accept sector as a filter
+@app.get(
+    "/quote/stock/weekly/{search_date}",
+    response_model=List[entity_schemas.Weekly_Stock_Quotes],
+)
+def show_records(
+    search_date: str,
+    db: Session = Depends(get_db),
+    filters: Optional[usecase_rules.stock_filter] = None,
+):
+    if filters == None:
+        return (
+            db.query(adapter_models.Weekly_Stock_Quotes)
+            .filter(adapter_models.Weekly_Stock_Quotes.quote_date == search_date)
+            .all()
+        )
+    elif filters.stock_code == None:
+        raise HTTPException(
+            status_code=400,
+            detail="Bad request. Acceptable Query parameter is 'stock_code'",
+        )
+    else:
+        return (
+            db.query(adapter_models.Weekly_Stock_Quotes)
+            .filter(adapter_models.Weekly_Stock_Quotes.quote_date == search_date)
+            .filter(
+                adapter_models.Weekly_Stock_Quotes.stock_code.in_(filters.stock_code)
+            )
+            .all()
+        )
--- test-rest-api.py
+++ test-rest-api.py
@@ -1,109 +1,151 @@
 import requests
 import json
 import logging, sys
+
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
+
 def startBanner(testNumber):
-    logging.info('=========================================')
-    logging.info(f'starting use case {testNumber}')
+    logging.info("=========================================")
+    logging.info(f"starting use case {testNumber}")
+
 
 def endBanner(testNumber):
-    logging.info(f'finished use case {testNumber}')
-    logging.info('=========================================')
+    logging.info(f"finished use case {testNumber}")
+    logging.info("=========================================")
 
 
 # use case 1
 def useCaseOne():
-    useCase = 'one'
+    useCase = "one"
     startBanner(useCase)
 
-    params = {
-        'stock_code': [ 'avh', 'bhp' ]
-    }
+    params = {"stock_code": ["avh", "bhp"]}
 
-    r = requests.get('http://127.0.0.1:8001/stocks', json=params)
+    r = requests.get("http://127.0.0.1:8001/stock", json=params)
     rjson = r.json()
     print(json.dumps(rjson, sort_keys=False, indent=4))
-    
+
     endBanner(useCase)
 
 
 # use case 2
 def useCaseTwo():
-    useCase = 'two'
+    useCase = "two"
     startBanner(useCase)
 
-    r = requests.get('http://127.0.0.1:8001/stocks/bhp')
+    r = requests.get("http://127.0.0.1:8001/stock/bhp")
     rjson = r.json()
     print(json.dumps(rjson, sort_keys=False, indent=4))
-    
+
     endBanner(useCase)
 
 
 # use case 3
 def useCaseThree():
-    useCase = 'three'
+    useCase = "three"
     startBanner(useCase)
 
-    r = requests.get('http://127.0.0.1:8001/stocks/14d/quotes/weekly')
+    r = requests.get("http://127.0.0.1:8001/stock/14d/quotes/weekly")
     rjson = r.json()
     print(json.dumps(rjson, sort_keys=False, indent=4))
-    
+
     endBanner(useCase)
 
 
 # use case 4
-def useCaseFour(dot, params = None):
-    useCase = 'Four' + dot
+def useCaseFour(dot, params=None):
+    useCase = "Four" + dot
     startBanner(useCase)
 
     if params == None:
-        r = requests.get('http://127.0.0.1:8001/stocks/14d/quotes/weekly/')
+        r = requests.get("http://127.0.0.1:8001/stock/14d/quotes/weekly")
     else:
-        r = requests.get('http://127.0.0.1:8001/stocks/14d/quotes/weekly/', json=params)
-    
+        r = requests.get("http://127.0.0.1:8001/stock/14d/quotes/weekly", json=params)
+
     rjson = r.json()
     print(json.dumps(rjson, sort_keys=False, indent=4))
-        
+
     endBanner(useCase)
 
-useCaseOne()
-useCaseTwo()
+
+# use case 5
+def useCaseFive(dot, params=None):
+    useCase = "Five" + dot
+    startBanner(useCase)
+
+    if params == None:
+        r = requests.get("http://127.0.0.1:8001/quote/weekly/stock")
+    else:
+        r = requests.get("http://127.0.0.1:8001/quote/weekly/stock", json=params)
+
+    rjson = r.json()
+    print(json.dumps(rjson, sort_keys=False, indent=4))
+
+    endBanner(useCase)
+
+
+# use case 6
+def useCaseSix(dot, params=None):
+    useCase = "Six" + dot
+    startBanner(useCase)
+
+    if params == None:
+        r = requests.get("http://127.0.0.1:8001/quote/stock/weekly/2021-03-15")
+    else:
+        r = requests.get(
+            "http://127.0.0.1:8001/quote/stock/weekly/2021-03-15", json=params
+        )
+
+    rjson = r.json()
+    print(json.dumps(rjson, sort_keys=False, indent=4))
+
+    endBanner(useCase)
+
+
+# useCaseOne()
+# useCaseTwo()
 useCaseThree()
-useCaseFour('1', params = {
-        'start_date': '2021-01-01'
-    }
-)
+# useCaseFour('1', params = {
+#        'start_date': '2021-01-01'
+#    }
+# )
 
-useCaseFour('2', params = {
-        'end_date': '2021-01-01'
-    }
-)
+# useCaseFour('2', params = {
+#        'end_date': '2021-01-01'
+#    }
+# )
 
-useCaseFour('3', params = {
-        'start_date': '2021-01-01',
-        'end_date': '2021-02-01'
-    }
-)
+# useCaseFour('3', params = {
+#        'start_date': '2021-01-01',
+#        'end_date': '2021-02-01'
+#    }
+# )
 
-useCaseFour('4', params = {
-        'period': '10d'
-    }
-)
+# useCaseFour('4', params = {
+#        'period': '10d'
+#    }
+# )
 
-useCaseFour('5', params = {
-        'period': '3w'
-    }
-)
+# useCaseFour('5', params = {
+#        'period': '3w'
+#    }
+# )
 
-useCaseFour('6', params = {
-        'period': '1m'
-    }
-)
+# useCaseFour('6', params = {
+#        'period': '1m'
+#    }
+# )
 
-useCaseFour('7', params = {
-        'period': '1y'
-    }
-)
+# useCaseFour('7', params = {
+#        'period': '1y'
+#    }
+# )
 
-useCaseFour('8', params = None)
+# useCaseFour('8', params = None)
+
+# useCaseFive("1", params=None)
+# useCaseFive("2", params={"stock_code": ["14d", "bhp"]})
+
+# useCaseSix("1")
+# useCaseSix("2", params={"stock_code": ["bhp"]})
